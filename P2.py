117c7f41e5611c8dd22f90180e95535a9c704db8
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2023-02-22 20:33:13 -0700

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,14 @@
+#33.2 LAB: Input: Mad Lib
+#Practice test Q2
+
+#read inputs
+first_name = input()
+generic_location = input()
+whole_number = int(input())
+plural_noun = input()
+
+#input a short story using the four input values.
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
+
+
+#This one is easy.
